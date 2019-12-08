963581803192a5d47765f58e8e1f1d2bed8f880a
Viktor Kudria <v.a.kudrya@gmail.com>
2019-12-08 16:37:57 +0200

    Sign In | In progress

---
+++ src/Tangerine.Web/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tangerine.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content("Hop");
+        }
+    }
+}
--- src/Tangerine.Web/Controllers/AuthController.cs
+++ src/Tangerine.Web/Controllers/AuthController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Tangerine.Core.Models.Auth;
 using Tangerine.Data;
 using Tangerine.Web.Models.Auth;
@@ -14,15 +21,60 @@ namespace Tangerine.Web.Controllers
             _appDbContext = appDbContext;
         }
 
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl = null)
         {
+            if (_appDbContext.Users.FirstOrDefaultAsync(x => true).Result == null)
+            {
+                _appDbContext.Users.Add(new User()
+                {
+                    Login = "admin",
+                    Password = "admin"
+                });
+
+                _appDbContext.SaveChanges();
+            }
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel viewModel)
+        public async Task<IActionResult> Login(LoginViewModel viewModel, string returnUrl)
         {
+            if (ModelState.IsValid)
+            {
+                var user = await _appDbContext.Users.FirstOrDefaultAsync(
+                    u => u.Login == viewModel.Login &&
+                            u.Password == viewModel.Password);
+
+                if (user != null)
+                {
+                    await Authenticate(user.Login);
+
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError("", "Password or login is incorrect");
+            }
+
             return View(viewModel);
+        }
+
+        private async Task Authenticate(string userName)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+            };
+
+            ClaimsIdentity id = new ClaimsIdentity(
+                claims,
+                "ApplicationCookie",
+                ClaimsIdentity.DefaultNameClaimType,
+                ClaimsIdentity.DefaultRoleClaimType);
+
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(id));
         }
     }
 }
